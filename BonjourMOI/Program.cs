a985d511f6e1ffb6235ef364d6a167b0009226e1
hp <hp@HP-MARDOCHE>
2025-06-22 04:11:41 +0200

    Ajoutez des fichiers projet.

---
+++ BonjourMOI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BonjourMoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quel est ton nom ? ");
+            string nom = Console.ReadLine();
+
+            Console.WriteLine($"Bonjour, {nom} ! Ravi de faire ta connaissance 👋");
+            Console.WriteLine("Appuie sur une touche pour quitter...");
+            Console.ReadKey();
+        }
+    }
+}
